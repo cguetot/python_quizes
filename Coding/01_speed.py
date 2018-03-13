47d3edaeac3e10d605da96811bb1293b3799f66e
Carlos Alberto Gueto-Tettay <carlosgueto@gmail.com>
2018-03-13 09:53:04 -0500

    Actividad de codigo 01. Calcular la velocidad

---
+++ Coding/01_speed.py
@@ -0,0 +1,31 @@
+# Para obtener ciertas estadísticas de un recorrido, se pide realizar un
+#  y en metros por segundo. programa que dada una distancia, entregue la
+# velocidad en kilómetros por hora. Para esto, existen dos variables tiempo
+# y distancia que vienen en segundos y kilómetros respectivamente. Tu programa
+# debe guardar en la variable resultado un string, por ejemplo,
+# para el siguiente caso:
+# tiempo = 1
+#
+# distancia = 0.01
+# La variable resultado debería tener lo siguiente:
+# "La velocidad es 36.0 km/h o 10.0 m/s"
+#
+# Para poder resolver este problema , debes escribir el código que falta en el
+# espacio que lo señala. Asume que ya existen variables con los nombres tiempo
+#  y distancia, que ya contienen. los valores requeridos (no debes pedírselos
+# al usuario), haz los cálculos que necesites, y luego deja el resultado en una
+# variable llamada resultado.
+
+
+def velocidad(distancia, tiempo):
+    # desde aquí hacia abajo debes modificar el programa
+    # modifica la variable resultado
+    # recuerda que los datos están en las variables distancia y tiempo
+    return resultado
+
+
+# Prueba de ejecución
+distancia_prueba = 0.01
+tiempo_prueba = 1
+resultado = velocidad(distancia_prueba, tiempo_prueba)
+print(resultado)
